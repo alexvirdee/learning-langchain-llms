806b3b5ab30e201dc131ce76c4b6d1471e9a2edb
Alex Virdee <alex.virdee08@gmail.com>
2024-06-22 21:55:02 -0400

    agent retrieving twitter username

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Running pipenv shell
 
 `pipenv shell`
+
--- ice_breaker/ice_breaker.py
+++ ice_breaker/ice_breaker.py
@@ -1,19 +1,12 @@
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
-
-information = """
-    Elon Reeve Musk is a businessman and investor known for his key roles in space company SpaceX and automotive company Tesla, Inc. Other involvements include ownership of X Corp., formerly Twitter, and his role in the founding of The Boring Company, xAI, Neuralink and OpenAI. He is one of the wealthiest people in the world; as of June 2024, Forbes estimates his net worth to be US$214 billion.[5]
-
-    Musk was born in Pretoria, a member of the South African Musk family, and briefly attended the University of Pretoria before immigrating to Canada at age 18, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at Queen's University at Kingston in Canada. Musk later transferred to the University of Pennsylvania and received bachelor's degrees in economics and physics. He moved to California in 1995 to attend Stanford University, but dropped out after two days and, with his brother Kimbal, co-founded online city guide software company Zip2. The startup was acquired by Compaq for $307 million in 1999. That same year, Musk co-founded X.com, a direct bank. X.com merged with Confinity in 2000 to form PayPal. In October 2002, eBay acquired PayPal for $1.5 billion. Using $100 million of the money he made from the sale of PayPal, Musk founded SpaceX, a spaceflight services company, in 2002.
-
-    In 2004, Musk was an early investor in electric vehicle manufacturer Tesla Motors, Inc. (later Tesla, Inc.). He became the company's chairman and product architect, assuming the position of CEO in 2008. In 2006, Musk helped create SolarCity, a solar-energy company that was acquired by Tesla in 2016 and became Tesla Energy. In 2013, he proposed a hyperloop high-speed vactrain transportation system. In 2015, he co-founded OpenAI, a nonprofit artificial intelligence research company. The following year, Musk co-founded Neuralink—a neurotechnology company developing brain–computer interfaces—and The Boring Company, a tunnel construction company. In 2018, the U.S. Securities and Exchange Commission (SEC) sued Musk, alleging that he had falsely announced that he had secured funding for a private takeover of Tesla. To settle the case, Musk stepped down as the chairman of Tesla and paid a $20 million fine. In 2022, he acquired Twitter for $44 billion. He subsequently merged the company into newly created X Corp. and rebranded the service as X the following year. In March 2023, Musk founded xAI, an artificial intelligence company.
-"""
+from third_parties.linkedin import scrape_linkedin_profile
 
 if __name__ == "__main__":
     print("Hello LangChain!")
 
     summary_template = """
-        given the information {information} about a person from I want you to create:
+        given th Linkedin information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
@@ -25,7 +18,7 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = summary_prompt_tenmplate | llm
-
-    res = chain.invoke(input={"information": information})
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="https://www.linkedin.com/in/alexvirdee/")
+    res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
